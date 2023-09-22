fe40123734c4f949bea4b73e1daeb6a4fdaab1dc
LarsDD0815 <132018238+LarsDD0815@users.noreply.github.com>
2023-09-22 22:46:38 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+bluetooth.onBluetoothConnected(function () {
+    bluetooth.startUartService()
+})
+bluetooth.onBluetoothDisconnected(function () {
+    mecanumRobotV2.MotorenAnhalten()
+})
+bluetooth.onUartDataReceived(serial.delimiters(Delimiters.Hash), function () {
+    mecanumRobotV2.stelleMotorenPerBluetooth(bluetooth.uartReadUntil(serial.delimiters(Delimiters.Hash)))
+})
 basic.forever(function () {
-	
+    bluetooth.uartWriteNumber(mecanumRobotV2.ultra())
 })
